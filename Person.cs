af3b6f2e7052b534a52f3ecfa42da474e81f562c
Albert <awilczynski@edu.cdv.pl>
2021-10-25 15:23:01 +0200

    Initial commit

---
+++ Person.cs
@@ -0,0 +1,10 @@
+namespace lab1
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phonenumber { get; set; }
+    }
+}
